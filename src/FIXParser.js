884dae96275b84791558a347c9d0348920198cd9
Victor Norgren <victor@logotype.se>
2017-06-15 21:06:23 +0800

    1.6.5 (performance improvements)

--- src/FIXParser.js
+++ src/FIXParser.js
@@ -11,6 +11,13 @@ import {Fields} from './fields/Fields';
 import {Enums} from './enums/Enums';
 import {Message} from './message/Message';
 
+const SOH = '\x01';
+const STRING_A = '^A ';
+const STRING_EQUALS = '=';
+const RE_A = /\^A\s|\^A/g;
+const RE_ESCAPE = /([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g; // eslint-disable-line no-useless-escape
+const RE_PIPE = /\|/g;
+
 export default class FIXParser extends EventEmitter {
 
     constructor() {
@@ -24,23 +31,29 @@ export default class FIXParser extends EventEmitter {
     preProcess(data) {
         this.message.reset();
 
-        let separator = '';
+        const firstSeparator = data.charCodeAt(9);
+        let separator = null;
 
-        if(data.charCodeAt(9) === 1) {
+        if(firstSeparator === 124) {
+            // | separator
+            separator = RE_PIPE;
+        } else if(firstSeparator === 1) {
             // SOH separator
-            this.processedData = this.message.string = data;
+            this.message.string = data;
+            this.processedData = data.split(SOH);
             return;
-        } else if(data.substr(9, 3) === '^A ') {
+        } else if(data.substring(9, 12) === STRING_A) {
             // Multi-character separator
-            separator = /\^A |\^A/g;
+            separator = RE_A;
         } else {
             // Determine separator
-            const escapedSeparatorCharacter = String.fromCharCode(data.charCodeAt(9)).replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g, '\\$&'); // eslint-disable-line no-useless-escape
+            const escapedSeparatorCharacter = String.fromCharCode(firstSeparator).replace(RE_ESCAPE, '\\$&');
             separator = new RegExp(escapedSeparatorCharacter, 'g');
         }
 
-        this.processedData = data.replace(separator, '\x01');
-        this.message.string = this.processedData;
+        const stringData = data.replace(separator, SOH);
+        this.message.string = stringData;
+        this.processedData = stringData.split(SOH);
     }
 
     parse(data) {
@@ -53,16 +66,14 @@ export default class FIXParser extends EventEmitter {
 
         this.preProcess(data);
 
-        const array = this.processedData.split('\x01');
-
-        for (i; i < array.length - 1; i++) {
+        for (i; i < this.processedData.length - 1; i++) {
 
             item = {};
 
-            equalsOperator = array[i].indexOf('=');
+            equalsOperator = this.processedData[i].indexOf(STRING_EQUALS);
 
-            item.tag = parseInt(array[i].substring(0, equalsOperator), 10);
-            value = array[i].substring(equalsOperator + 1);
+            item.tag = parseInt(this.processedData[i].substring(0, equalsOperator), 10);
+            value = this.processedData[i].substring(equalsOperator + 1);
 
             this.fields.processField(this.message, item, item.tag, value);
             this.enums.processEnum(item, item.tag, value);
--- src/message/Message.js
+++ src/message/Message.js
@@ -1,12 +1,18 @@
+const TAG_CHECKSUM = '10=';
+const TAG_MSGTYPE = '35=';
+
 export class Message {
 
-    constructor() {
+    static pad(value, size) {
+        const paddedString = `00${value}`;
+        return paddedString.substr(paddedString.length - size);
+    }
 
+    constructor() {
         this.data = [];
         this.string = '';
         this.bodyLengthValid = false;
         this.checksumValid = false;
-
         this.checksumValue = null;
         this.checksumExpected = null;
         this.bodyLengthValue = null;
@@ -25,8 +31,8 @@ export class Message {
     }
 
     validateBodyLength(value) {
-        const startLength = this.string.indexOf('35=') === -1 ? 0 : this.string.indexOf('35='),
-        endLength = this.string.indexOf('10=') === -1 ? this.string.length : this.string.indexOf('10='),
+        const startLength = this.string.indexOf(TAG_MSGTYPE) === -1 ? 0 : this.string.indexOf(TAG_MSGTYPE),
+        endLength = this.string.indexOf(TAG_CHECKSUM) === -1 ? this.string.length : this.string.indexOf(TAG_CHECKSUM),
         bodyLength = endLength - startLength;
 
         this.bodyLengthValue = parseInt(value, 10);
@@ -35,23 +41,17 @@ export class Message {
         return this.bodyLengthValid;
     }
 
-    static pad(value, size) {
-        const paddedString = `00${value}`;
-        return paddedString.substr(paddedString.length - size);
-    }
-
     validateChecksum(value) {
-        const length = this.string.indexOf('10=') === -1 ? this.string.length : this.string.indexOf('10='),
-            data = this.string.substr(0, length);
+        const length = this.string.indexOf(TAG_CHECKSUM) === -1 ? this.string.length : this.string.indexOf(TAG_CHECKSUM),
+            data = this.string.substring(0, length);
 
-        let integerValues = 0,
-            paddedValue = '';
+        let integerValues = 0;
 
-        for(let i = 0; i < data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
             integerValues += data.charCodeAt(i);
         }
 
-        paddedValue = Message.pad(integerValues & 255, 3);
+        const paddedValue = Message.pad(integerValues & 255, 3);
 
         this.checksumValue = value;
         this.checksumExpected = paddedValue;
